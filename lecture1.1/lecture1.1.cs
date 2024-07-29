8cb2c74fab7f3f901d9d748aec66b4a06cc2bdad
bak88 <dimbak1990@gmail.com>
2024-07-29 13:18:39 +0300

    testCommit

---
+++ lecture1.1/lecture1.1.cs
@@ -0,0 +1,18 @@
+﻿namespace Dive_into_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = Dived2Numbers(2, 1);
+            Console.WriteLine(res);
+        }
+
+        static int Dived2Numbers(int a, int b)
+        {
+            int result = a / b;
+            return result;
+        }
+
+    }
+}
---
+++ seminar1.1/Seminar1.1.cs
@@ -0,0 +1,11 @@
+﻿namespace Seminar1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"args[0] = {args[0]}");
+
+        }
+    }
+}
