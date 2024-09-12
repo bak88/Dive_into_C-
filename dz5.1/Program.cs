a3c10fc479a6cd013829163354e05361745df345
bak88 <dimbak1990@gmail.com>
2024-09-12 15:28:33 +0300

    dz5.1

--- dz5.1/Program.cs
+++ dz5.1/Program.cs
@@ -2,9 +2,69 @@
 {
     internal class Program
     {
+        // Доработайте приложение поиска пути в лабиринте,
+        // но на этот раз вам нужно определить сколько всего выходов имеется в лабиринте
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stack<Tuple<int, int>> stack = new Stack<Tuple<int, int>>();
+
+            int[,] labirynth1 = new int[,]
+            {
+                {1, 1, 1, 1, 1, 2, 1 },
+                {1, 0, 0, 0, 0, 0, 2 },
+                {1, 0, 1, 1, 1, 0, 1 },
+                {1, 0, 0, 0, 1, 0, 2 },
+                {1, 1, 0, 0, 1, 1, 1 },
+                {1, 1, 1, 1, 1, 1, 1 },
+                {1, 1, 1, 1, 1, 1, 1 }
+            };
+
+
+            FindPath(4, 3);
+
+
+            int FindPath(int i, int j)
+            {
+                int countExit = 0;
+
+                Console.WriteLine(labirynth1[i, j]);
+
+                if (labirynth1[i, j] == 0) stack.Push(new(i, j));
+
+                while (stack.Count > 0)
+                {
+                    var current = stack.Pop();
+
+                    Console.WriteLine($"{current.Item1},{current.Item2} ");
+
+                    if (labirynth1[current.Item1, current.Item2] == 2)
+                    {
+                        countExit++;
+                        Console.WriteLine($"----{countExit} Путь найден {current.Item1},{current.Item2} ");
+                    }
+
+                    labirynth1[current.Item1, current.Item2] = 1;
+
+                    if (current.Item1 + 1 < labirynth1.GetLength(0)
+                    && labirynth1[current.Item1 + 1, current.Item2] != 1)
+                        stack.Push(new(current.Item1 + 1, current.Item2));
+
+                    if (current.Item2 + 1 < labirynth1.GetLength(1) &&
+                    labirynth1[current.Item1, current.Item2 + 1] != 1)
+                        stack.Push(new(current.Item1, current.Item2 + 1));
+
+                    if (current.Item1 > 0 && labirynth1[current.Item1 - 1, current.Item2] != 1)
+                        stack.Push(new(current.Item1 - 1, current.Item2));
+
+                    if (current.Item2 > 0 && labirynth1[current.Item1, current.Item2 - 1] != 1)
+                        stack.Push(new(current.Item1, current.Item2 - 1));
+                }
+
+                if(countExit == 0)
+                    Console.WriteLine("Exit closed");
+                return countExit;
+            }
         }
     }
 }
