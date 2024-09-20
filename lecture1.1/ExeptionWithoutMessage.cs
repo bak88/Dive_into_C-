052601f62bae66fb5ce6f948c9036cd208ffcdc3
bak88 <dimbak1990@gmail.com>
2024-09-20 15:16:34 +0300

    lecture8

---
+++ lecture1.1/ExeptionWithoutMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Погружение_в_С_
+{
+    class ExeptionWithoutMessage
+    {
+    }
+}
---
+++ lecture8.1/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace lecture8._1
+{
+    internal class Program
+    {
+        static int DivideTwoNumber(int a, int b)
+        {
+            return a / b;
+
+        }
+
+        static int[] DivideTwoArray(int[] a, int[] b)
+        {
+            int[] res = new int[a.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                res[i] = a[i] / b[i];
+            }
+            return res;
+        }
+
+        static void RunDivideTwoArray(int[] a, int[] b)
+        {
+            try
+            {
+                var res = DivideTwoArray(a, b);
+                foreach (var item in res)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine(" Арифметическая ошибка");
+                if (ex is DivideByZeroException)
+                {
+                    Console.WriteLine(" Ошибка деления на ноль");                    
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(" Ошибка длинны массива");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Не могу обработать ошибку");
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Console.WriteLine(DivideTwoNumber(1, 0));
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Error divide by zero " + e);
+
+            //}
+
+
+
+
+
+            //AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) =>
+            //{
+            //    Console.WriteLine("---------Возможно исключение: " + eventArgs.Exception);
+            //};
+
+            //AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
+            //{
+            //    Console.WriteLine("---------Программа не обработала исключение: " + eventArgs.ExceptionObject);
+            //};
+
+
+
+
+
+
+
+
+            //RunDivideTwoArray(new int[] { 8, 2, 4 }, new int[] { 2, 2 });
+            //RunDivideTwoArray(new int[] { 8, 2, 4 }, new int[] { 0, 2, 2 });
+            // RunDivideTwoArray(null, new int[] { 2, 2, 2 });
+            
+
+
+            throw new Exception("Демонстрация сообщения об ошибке");
+
+
+
+
+        }
+    }
+}
+
+
+
